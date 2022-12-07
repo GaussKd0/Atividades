1aae2aee4954e649e40a621d63710a88fdbe4c78
Pedro Galvão <93468007+GaussKd0@users.noreply.github.com>
2022-12-06 23:58:55 -0300

    Update Calca.java

--- Atividade17/src/Calca.java
+++ Atividade17/src/Calca.java
@@ -1,6 +1,27 @@
-public class Calca extends Blusa{
-    @Override
-    public String setCor(String Cor) {
-        return super.setCor("preto");
+public class Calca extends Roupa{
+    private String Modelo;
+    private String Genero;
+
+    public String getGenero() {
+
+        return this.Genero;
+    }
+
+    public String getTipoModelo(){
+
+        return  this.Modelo;
+    }
+
+    public void setGenero(String Genero) {
+        this.Genero = Genero;
+    }
+
+    public void setTipoDaCalca(String Modelo){
+
+        this.Modelo = Modelo;
+    }
+
+    public void Caracteristicas(){
+        System.out.println("\nCaracteristicas Da Calça:\nCor Da Calça: " + super.getCor() + "\nTamanho Da Calça: " + super.getTamanho() + "\nMaterial Da Calça: " + super.getMaterial() + "\nTipo Da Calça: " + getTipoModelo() + "\nMarca Da Calça " + getGenero());
     }
 }
