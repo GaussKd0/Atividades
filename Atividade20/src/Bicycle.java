6c0013ab6a30542feffa20397d19ef50719756d7
Pedro Galvão <93468007+GaussKd0@users.noreply.github.com>
2022-12-15 15:39:36 -0300

    Update Bicycle.java

--- Atividade20/src/Bicycle.java
+++ Atividade20/src/Bicycle.java
@@ -1,3 +1,8 @@
-public class Bicycle {
+public class Bicycle implements CarbonFootprint {
+    private int pegadasCarbono;
 
+    public int getCarbonFootprint() {
+        pegadasCarbono = 0;
+        return pegadasCarbono;
+    }
 }
